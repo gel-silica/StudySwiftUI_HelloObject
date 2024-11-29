1e59d6db5477ae645a6ea33ed39aac1281e8880c
silicagel <ksk19981004@gmail.com>
2024-11-29 20:50:41 +0900

    Initial Commit

---
+++ HelloObject/HelloObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloObjectApp.swift
+//  HelloObject
+//
+//  Created by silicagel on 2024/11/29.
+//
+
+import SwiftUI
+
+@main
+struct HelloObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
